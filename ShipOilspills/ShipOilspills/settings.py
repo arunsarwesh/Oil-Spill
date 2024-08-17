1584a64e90bd6895ebead84285b304c7a81de4fc
arunsarwesh <sarweshwarsarweshwar635@gmail.com>
2024-08-17 22:56:33 +0530

    init

--- ShipOilspills/ShipOilspills/settings.py
+++ ShipOilspills/ShipOilspills/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'ShipOilspills.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / "templates"],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -117,6 +117,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "Ships\static",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
--- ShipOilspills/Ships/views.py
+++ ShipOilspills/Ships/views.py
@@ -1,3 +1,4 @@
 from django.shortcuts import render
-
+def main(request):
+    return render(request,'stat.html')
 # Create your views here.
