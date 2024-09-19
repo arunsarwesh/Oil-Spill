406a4e0aebc95d865ab5d8c896ce07ba50a24eef
SarweshwarDeivasihamani <89565470+arunsarwesh@users.noreply.github.com>
2024-09-19 08:42:36 +0530

    Update urls.py

--- ShipOilspills/urls.py
+++ ShipOilspills/urls.py
@@ -21,7 +21,7 @@ from Ships import views
 urlpatterns = [
     
     path('admin/', admin.site.urls),
-    path('', views.AIS, name="AIS"),
+    path('https://arunsarwesh13.pythonanywhere.com/', views.AIS, name="AIS"),
     path('map/', views.map, name="map"),
     
 ]
